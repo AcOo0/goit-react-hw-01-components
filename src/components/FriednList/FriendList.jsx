6fa8e8688e486bdc0c71738bc3c18c621382982d
AcOo0 <acoo.012@gmail.com>
2023-12-24 11:56:27 +0200

    Fix FriendList.jsx FriendListItem.jsx

--- src/components/FriednList/FriendList.jsx
+++ src/components/FriednList/FriendList.jsx
@@ -1,14 +1,17 @@
 import styles from './friendList.module.css'
+import FriendListItem from './FriendListItem';
 
 const FriendList = ({ friends = [] }) => {
-    const friendListItem = friends.map(({id, avatar, name, isOnline}) => (<li key={id} className={styles.item}>
-                                                    <span className={isOnline ? `${styles.status} ${styles.active}` : styles.status}></span>
-                                                    <img className={styles.avatar} src={avatar} alt="User avatar" width="48" />
-                                                    <p className={styles.name}>{name}</p>
-                                                    </li>))
+    
     return (
         <ul className={styles.friendList}>
-            {friendListItem}
+            {friends.map(friend => (<FriendListItem
+                key={friend.id}
+                avatar={friend.avatar}
+                name={friend.name}
+                isOnline={friend.isOnline}
+                />
+                ))}
         </ul>
     );
 };
---
+++ src/components/FriednList/FriendListItem.jsx
@@ -0,0 +1,10 @@
+import FriendList from './FriendList';
+import styles from './friendList.module.css'
+
+export default function FriendListItem({ avatar, name, isOnline }) {
+    return (<li className={styles.item}>
+        <span className={isOnline ? `${styles.status} ${styles.active}` : styles.status}></span>
+        <img className={styles.avatar} src={avatar} alt="User avatar" width="48" />
+        <p className={styles.name}>{name}</p>
+    </li>);
+}
